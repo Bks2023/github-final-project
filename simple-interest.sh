e859b317825cdef9ba5375225d03e7565e213d76
singh191 <122477187+singh191@users.noreply.github.com>
2023-01-14 02:12:44 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,9 +4,9 @@
 
    # Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
+   # Author: Bikas Singh
    # Additional Authors:
-   # <your GitHub username>
+   # singh191
 
    # Input:
    # p, principal amount
